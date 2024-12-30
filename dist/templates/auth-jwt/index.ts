c70454dd58e375c30118b67bdbdfa3468666af27
ziadhm001 <ziadh.m001@gmail.com>
2024-12-30 12:31:35 +0200

    Remove dist folder and add to .gitignore

--- dist/templates/auth-jwt/index.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from 'express';
-import { authRoutes } from '../../routes/auth.routes';
-
-export function init(app: Application): void {
-  app.use('/auth', authRoutes);
-  console.log('Auth module initialized at /auth');
-}
